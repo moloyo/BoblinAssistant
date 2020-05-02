297bca6ec14c9aba250e25bfaa3203a6fad4ffba
Slakkan <lnpastinante@gmail.com>
2020-05-02 20:03:19 -0300

    mockup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DiceTableModule } from './dice-table/dice-table.module';
 
 @NgModule({
   declarations: [
@@ -10,7 +11,8 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    DiceTableModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/dice-table/dice-table.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-dice-table',
+  templateUrl: './dice-table.component.html',
+  styleUrls: ['./dice-table.component.scss']
+})
+export class DiceTableComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
