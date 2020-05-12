4d27e76170e1dc3acb0c0a8d99726bab397e0d88
Slakkan <lnpastinante@gmail.com>
2020-05-11 21:35:27 -0300

    finished all dice prefabs, added throw functionallity and comunication from unity to javascript

--- src/app/dice-table/dice-table.component.ts
+++ src/app/dice-table/dice-table.component.ts
@@ -1,15 +1,27 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, AfterViewInit } from '@angular/core';
+declare var UnityLoader: any;
 
 @Component({
   selector: 'app-dice-table',
   templateUrl: './dice-table.component.html',
   styleUrls: ['./dice-table.component.scss']
 })
-export class DiceTableComponent implements OnInit {
+export class DiceTableComponent implements AfterViewInit {
+  gameInstance: any;
 
   constructor() { }
 
-  ngOnInit(): void {
+  ngAfterViewInit(): void {
+    this.gameInstance = UnityLoader.instantiate('gameContainer', 'assets/unity/Build/Build.json');
+    window.addEventListener('result', (e: CustomEvent) => {
+      console.log(e.detail);
+    });
+    window.addEventListener('roll', (e: CustomEvent) => {
+      console.log(e.detail);
+    });
   }
 
+  setFullscreen() {
+    this.gameInstance.SetFullscreen(1);
+  }
 }
